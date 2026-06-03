e0ced7ff64d7796f9ba676916ad4aa84220afa03
RaceproxateDev <coxemeftigtipout@gmail.com>
2026-06-03 18:16:51 +0200

    Refactor boolean library with additional methods

--- bool.js
+++ bool.js
@@ -8,12 +8,11 @@
 
   // prototype
   P = {},
-  // Booleans [custom]
+    // Booleans [custom]
   B = {},
-  // Constants
+    // Constants
   Q = {};
 
-  // Examples
   B.Maybe = 0.5;
   B.almostNever = 0.1;
   B.notSure = 0.6;
@@ -70,19 +69,69 @@
 
   P.equalsTo = P.eq = function (y) {
     return this.cmp(y) === 0
-  };
+  }
 
   Q.equalsTo = Q.eq = function (x,y) {
     return new bool(x).eq(y)
-  };
+  }
 
   P.notEqualsTo = P.neq = function (y) {
     return this.cmp(y) !== 0
-  };
+  }
 
   Q.notEqualsTo = Q.neq = function (x,y) {
     return new bool(x).neq(y)
-  };
+  }
+
+  P.toNumber = P.toNum = function () {
+    if (this.val === 1) return 1;
+    if (this.val === false) return 0;
+    if (Number.isNaN(this.val)) return NaN;
+
+    if (typeof this.val === 'number') return this.val;
+  }
+
+  P.subtract = P.sub = function (otherBool) {
+    if (!(otherBool instanceof bool)) otherBool=new bool(otherBool)
+    if (this.val === 0) return otherBool;
+    if (otherBool === 0) return this.val;
+    if (this.val-otherBool===0) return false;
+    if (this.val-otherBool===1) return true;
+    if (Number.isNaN(this.val)||Number.isNaN(otherBool)) return NaN;
+
+    var r = this.val.toNum() - otherBool.toNum()
+
+    return r
+  }
+
+  Q.subtract = Q.sub = function(x,y) {
+    return new bool(x).sub(y)
+  }
+
+  P.negate = P.neg = function() {
+    return this.val.toNum() * -1
+  }
+
+  Q.negate = Q.neg = function() {
+    var x = this.clone()
+    return new bool(x).neg()
+  }
+
+  P.addition = P.add = function(otherBool) {
+    return this.val.sub(otherBool.neg())
+  }
+
+  Q.addition = Q.add = function(x,y) {
+    return new bool(x).add(y)
+  }
+
+  P.formatPercent = P.toPercent = function (decimals=2) {
+    return (this.val * 100).toFixed(decimals) + "%";
+  }
+
+  Q.formatPercent = Q.toPercent = function (x, decimals=2) {
+    return new bool(x).toPercent(decimals)
+  }
 
   function clone(x) {
     var i, k
